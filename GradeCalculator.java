a3a9d74f5ea79dd0cb86e8bf16e8499229c59bc2
aaron2425 <antgo106@student.hbuhsd.edu>
2024-09-16 10:58:29 -0700

    done lol rizz

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,10 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
+    String course = "AP CS A";
+
+    int time = 120;
+
     int hw = 60;
     int hw2 = 45;
     int hw3 = 55;
@@ -17,13 +21,24 @@ public class GradeCalculator
     double finalWeight = (0.50);
 
     double finalGrade = ((avgHw * hwWeight) + (avgQuiz * quizWeight) + (final1 * finalWeight));
+    int weeklyTime = (time / 60);
     int finalGrade2 = (int) (finalGrade + 0.5);
-    System.out.println(
-    System.out.println(
-    System.out.println(
-    System.out.println(
-    System.out.println(
-    System.out.println(
-    System.out.println(finalGrade2);
+    System.out.println("course name: " + course);
+    System.out.println("average time spent: " + time);
+    System.out.println("homework grades for this course:");
+    System.out.println(hw);
+    System.out.println(hw2);
+    System.out.println(hw3);
+    System.out.println(hw4);
+    System.out.println("quiz grades for this course:");
+    System.out.println(quiz1);
+    System.out.println(quiz2);
+    System.out.println("final exam grade:");
+    System.out.println(final1);
+    System.out.println("Weekly time spent: " + weeklyTime + " hours");
+    System.out.println("average homework grade: " + avgHw);
+    System.out.println("average quiz grade: " +avgQuiz);
+    System.out.println("Final exam grade: " + final1);
+    System.out.println("overall grade: " + finalGrade2);
   }
 }
